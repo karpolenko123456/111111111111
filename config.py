aa1bc92ccaaa86e7eaa595c1158d643e0cafa687
karpolenko123456 <karpolenko123456@gmail.com>
2026-04-12 01:56:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-token = '6300311826:AAEn6v0J3EFnhLfhCWZllsN3Qt-uAeb5TJQ'
+token = '6562425766:AAGAtlcdAPZIcX69jg5C3ABG0ysUigDokes'
 # token = '7308900753:AAGwczi4FvLx2ehysc8xdAEjGzP5FiXm9wA'
 admin_id = 5978617406
 manager_link = f'https://t.me/MasonsFX_bot'
