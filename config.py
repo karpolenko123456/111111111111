ea953c163db4c45ecdfff0bc0f1c87c238e2c4df
karpolenko123456 <karpolenko123456@gmail.com>
2026-04-13 00:49:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ token = '6562425766:AAGAtlcdAPZIcX69jg5C3ABG0ysUigDokes'
 # token = '7308900753:AAGwczi4FvLx2ehysc8xdAEjGzP5FiXm9wA'
 admin_id = 5978617406
 manager_link = f'https://t.me/MasonsFX_bot'
-
+# token_cryptobot = '369546:AAxPmfahjiLrKIIgDNzwLtkhtlVjtIl1SPi'
+token_cryptobot = '369546:AAxPmfahjiLrKIIgDNzwLtkhtlVjtIl1SPi'
 stats_id = -1002522176326
